eb831b5e2bd1301b68113dfd56c6e9abcdc599a2
fsj <fsjasd123456@gmail.com>
2024-05-11 12:59:11 +0800

    拆分仓库

---
+++ src/views/Node/NodeControl.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "NodeControl"
+}
+</script>
+
+<template>
+  <v-card title="节点监控" class="pageMain-card">
+    <v-card-text>
+    </v-card-text>
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>
