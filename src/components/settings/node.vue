cf5582c0e8f9c320db76d3e2d2a2a872f0c9bef2
fsj <fsjasd123456@gmail.com>
2024-06-17 11:39:20 +0800

    修正语法

--- src/components/settings/node.vue
+++ src/components/settings/node.vue
@@ -16,19 +16,28 @@ export default {
     <div class="text-caption">
       节点运行数据上传间隔（秒）
     </div>
-    <v-text-field type="number" v-model.number="setting_data.node_usage.upload_data_interval"></v-text-field>
+    <v-text-field
+      type="number"
+      v-model.number="setting_data.node_usage.upload_data_interval"
+    ></v-text-field>
   </div>
   <div>
     <div class="text-caption">
       节点运行数据保存间隔（分）
     </div>
-    <v-text-field type="number" v-model.number="setting_data.node_usage.data_save_interval"></v-text-field>
+    <v-text-field
+      type="number"
+      v-model.number="setting_data.node_usage.data_save_interval"
+    ></v-text-field>
   </div>
   <div>
     <div class="text-caption">
       节点运行数据保存时间（天）
     </div>
-    <v-text-field type="number" v-model.number="setting_data.node_usage.data_storage_time"></v-text-field>
+    <v-text-field
+      type="number"
+      v-model.number="setting_data.node_usage.data_storage_time"
+    ></v-text-field>
   </div>
   <v-divider/>
   <p class="text-h5 setting_subtitle">节点默认告警设置</p>
@@ -36,55 +45,88 @@ export default {
     <div class="text-caption">
       启用告警
     </div>
-    <v-checkbox v-model="setting_data.node_default_alarm_setting.enable"></v-checkbox>
+    <v-switch
+      v-model="setting_data.node_default_alarm_setting.enable"
+      color="primary"
+      density="compact"
+    ></v-switch>
   </div>
   <div>
     <div class="text-caption">
       告警延迟时间
     </div>
-    <v-text-field type="number" v-model.number="setting_data.node_default_alarm_setting.delay_seconds"></v-text-field>
+    <v-text-field
+      type="number"
+      v-model.number="setting_data.node_default_alarm_setting.delay_seconds"
+      color="primary"
+      density="compact"
+    ></v-text-field>
   </div>
   <div>
     <div class="text-caption">
       CPU告警启用
     </div>
-    <v-checkbox v-model="setting_data.node_default_alarm_setting.cpu__enabled"></v-checkbox>
+    <v-switch
+      v-model="setting_data.node_default_alarm_setting.cpu__enable"
+      color="primary"
+      density="compact"
+    ></v-switch>
   </div>
   <div>
     <div class="text-caption">
       CPU告警阈值
     </div>
-    <v-text-field type="number" v-model.number="setting_data.node_default_alarm_setting.cpu__enabled"></v-text-field>
+    <v-text-field
+      type="number"
+      v-model.number="setting_data.node_default_alarm_setting.cpu__enable"
+    ></v-text-field>
   </div>
   <div>
     <div class="text-caption">
       内存告警启用
     </div>
-    <v-checkbox v-model="setting_data.node_default_alarm_setting.memory__enabled"></v-checkbox>
+    <v-switch
+      v-model="setting_data.node_default_alarm_setting.memory__enable"
+      color="primary"
+      density="compact"
+    ></v-switch>
   </div>
   <div>
     <div class="text-caption">
       内存告警阈值
     </div>
-    <v-text-field type="number" v-model.number="setting_data.node_default_alarm_setting.memory__threshold"></v-text-field>
+    <v-text-field
+      type="number"
+      v-model.number="setting_data.node_default_alarm_setting.memory__threshold"
+    ></v-text-field>
   </div>
   <div>
     <div class="text-caption">
       网络告警启用
     </div>
-    <v-checkbox v-model="setting_data.node_default_alarm_setting.network__enable"></v-checkbox>
+    <v-switch
+      v-model="setting_data.node_default_alarm_setting.network__enable"
+      color="primary"
+      density="compact"
+    ></v-switch>
   </div>
   <div>
     <div class="text-caption">
       发送数据包阈值
     </div>
-    <v-text-field type="number" v-model.number="setting_data.node_default_alarm_setting.network__send_threshold"></v-text-field>
+    <v-text-field
+      type="number"
+      v-model.number="setting_data.node_default_alarm_setting.network__send_threshold"
+    ></v-text-field>
   </div>
   <div>
     <div class="text-caption">
       接收数据包阈值
     </div>
-    <v-text-field type="number" v-model.number="setting_data.node_default_alarm_setting.network__receive_threshold"></v-text-field>
+    <v-text-field
+      type="number"
+      v-model.number="setting_data.node_default_alarm_setting.network__receive_threshold"
+    ></v-text-field>
   </div>
 </template>
 
