422970ede68736e11aef6d02335c0275e6926999
fsj <fsjasd123456@gmail.com>
2024-06-22 10:54:19 +0800

    CPU告警测试

---
+++ src/components/UserInfo/OTP_bind.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "OTP_bind"
+}
+</script>
+
+<template>
+  <v-card title="OTP手机令牌">
+    <v-card-text>
+      <v-container>
+        <span><v-icon icon="mdi:mdi-alert" size="32px" color="error"></v-icon> 未绑定OTP手机令牌</span>
+        <v-btn color="primary" size="small">去绑定</v-btn>
+      </v-container>
+    </v-card-text>
+  </v-card>
+</template>
+
+<style scoped>
+  .v-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
--- src/layouts/UserInfo/user_info_edit_layout.vue
+++ src/layouts/UserInfo/user_info_edit_layout.vue
@@ -1,26 +1,48 @@
 <script>
 import ChangePassword from "@/components/UserInfo/changePassword.vue";
 import EditUserInfo from "@/components/UserInfo/editUserInfo.vue";
+import otp_bind from "@/components/UserInfo/OTP_bind.vue";
 
 export default {
   name: "UserInfoEdit",
-  components: {EditUserInfo, ChangePassword}
+  components: {otp_bind, EditUserInfo, ChangePassword}
 }
 </script>
 
 <template>
-  <v-row>
-    <v-col>
+  <div class="userInfoEdit">
+    <div class="right">
       <EditUserInfo/>
-    </v-col>
-    <v-col cols="5">
+    </div>
+    <div class="left">
       <change-password/>
-    </v-col>
-  </v-row>
+      <otp_bind/>
+    </div>
+  </div>
 
 
 </template>
 
 <style scoped>
+.userInfoEdit {
+  display: flex;
+  justify-content: space-between;
+  gap: 15px;
+  .right {
+    flex: 2;
+  }
 
+  .left {
+    flex: 1;
+    div:not(:last-child) {
+      margin-bottom: 15px;
+    }
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  .userInfoEdit {
+    flex-direction: column;
+  }
+}
 </style>
