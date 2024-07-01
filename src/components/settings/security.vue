48fbfcf0ec28d279bd2eb069b66709ce1cf2341e
fsj <fsjasd123456@gmail.com>
2024-07-01 10:47:14 +0800

    密码等级限制

--- src/components/settings/security.vue
+++ src/components/settings/security.vue
@@ -6,12 +6,45 @@ export default {
       type: Object,
       required: true,
     }
+  },
+  data() {
+    return {
+      password_level: [
+        {
+          level: 1,
+          info: "Level 1 无限制(仅限制长度 6-16位)"
+        },
+        {
+          level: 2,
+          info: "Level 2 弱密码(限制数字+英文 6-16位)"
+        },
+        {
+          level: 3,
+          info: "Level 3 中密码(限制数字+英文大小写 8-20位)"
+        },
+        {
+          level: 4,
+          info: "Level 4 强密码(限制数字+英文大小写+特殊符号 8-20位)"
+        }
+      ]
+    }
   }
 }
 </script>
 
 <template>
   <p class="text-h5 setting_subtitle">安全性设置</p>
+  <div>
+    <div class="text-caption">
+      密码安全等级设置
+    </div>
+    <v-select
+      v-model.number="setting_data.security.password_level"
+      :items="password_level"
+      item-title = 'info'
+      item-value = 'level'
+    ></v-select>
+  </div>
   <div>
     <div class="text-caption">
       消息验证码重发时间(秒)
