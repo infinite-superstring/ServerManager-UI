00ec8950f9babcb83973ea62319e12a296940cc6
fsj <fsjasd123456@gmail.com>
2024-07-01 10:14:04 +0800

    error-网站监控

---
+++ src/components/web_status/WebStatusCode.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: "WebStatusCode",
+  props: ['code']
+}
+</script>
+
+<template>
+  <span
+    :style="{color:'green'}"
+    v-if="/^2/.test(code)">
+    <v-icon
+      size="x-small">
+      mdi-check-circle-outline
+    </v-icon>
+    在线
+  </span>
+  <span
+    :style="{color:'orange'}"
+    v-if="/^3/.test(code)">
+    <v-icon size="x-small">
+      mdi-help-circle-outline
+    </v-icon>
+    警告
+  </span>
+  <span
+    :style="{color:'red'}"
+    v-if="/^4/.test(code)">
+    <v-icon size="x-small">
+      mdi-close-circle-outline
+    </v-icon>
+    离线
+  </span>
+  <span
+    :style="{color:'red'}"
+    v-if="/^5/.test(code)">
+    <v-icon size="x-small">
+      mdi-close-circle-outline
+    </v-icon>
+    错误
+  </span>
+</template>
+
+<style scoped>
+
+</style>
