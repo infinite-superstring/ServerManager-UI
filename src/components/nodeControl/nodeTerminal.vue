9bffbfce098dace4248bc71d477e30eac3af85d4
2659848223 <2659848223@qq.com>
2024-07-03 15:39:46 +0800

    修复节点终端登录

--- src/components/nodeControl/nodeTerminal.vue
+++ src/components/nodeControl/nodeTerminal.vue
@@ -1,115 +1,119 @@
 <script>
-import {Terminal} from "@xterm/xterm";
-import {FitAddon} from "xterm-addon-fit/src/FitAddon";
+import {Terminal} from "xterm";
+import {FitAddon} from "xterm-addon-fit";
+import "xterm/css/xterm.css";
 import NodeOfflineOverlay from "@/components/nodeControl/nodeOfflineOverlay.vue";
+import TerminalLogin from "@/components/nodeControl/terminalLogin.vue";
 
-import("@xterm/xterm/css/xterm.css")
-
-
-let terminal
-let fitAddon
+let terminal;
+let fitAddon;
 
 export default {
   name: "nodeTerminal",
-  components: {NodeOfflineOverlay},
+  components: {
+    TerminalLogin,
+    NodeOfflineOverlay,
+  },
   props: {
     ws: {
       type: Object,
-      required: true,
+      required: true
     },
     node_uuid: {
       type: String,
-      required: true,
+      required: true
     },
     online: {
       type: Boolean,
-      required: true,
+      required: true
+    },
+    os:{
+      type: String,
+      required: true
     }
   },
   data: () => {
-    return {}
+    return {
+      resend: '',
+      terminal_ready: false
+    };
   },
   mounted() {
-    this.send({
-      action: 'terminal:connect'
-    })
-    if (this.online) {
+    if (this.os==="Windows"){
+      this.terminal_ready=true
+      this.windows_login()
       this.open_terminal()
     }
+    console.log(this.os,this.terminal_ready)
+    if (!this.terminal_ready){
+      this.check_login();
+    }
   },
-  unmounted() {
+  beforeUnmount() {
     this.send({
       action: 'terminal:close'
-    })
+    });
   },
   methods: {
     open_terminal() {
       terminal = new Terminal({
-        rendererType: "canvas", //渲染类型
-        convertEol: true, //启用时，光标将设置为下一行的开头
+        rendererType: "canvas",
+        convertEol: true,
         cursorBlink: true,
         macOptionIsMeta: true,
         theme: {
-          background: '#272822', // 背景色
-          foreground: '#F8F8F2', // 前景色
-          cursor: '#F8F8F2', // 光标颜色
-          selection: 'rgba(248, 248, 242, 0.3)', // 选中文本颜色
-          black: '#272822', // 基本黑色
-          red: '#F92672', // 基本红色
-          green: '#A6E22E', // 基本绿色
-          yellow: '#F4BF75', // 基本黄色
-          blue: '#66D9EF', // 基本蓝色
-          magenta: '#AE81FF', // 基本紫色
-          cyan: '#A1EFE4', // 基本青色
-          white: '#F8F8F2', // 基本白色
-          brightBlack: '#75715E', // 亮黑色
-          brightRed: '#F92672', // 亮红色
-          brightGreen: '#A6E22E', // 亮绿色
-          brightYellow: '#F4BF75', // 亮黄色
-          brightBlue: '#66D9EF', // 亮蓝色
-          brightMagenta: '#AE81FF', // 亮紫色
-          brightCyan: '#A1EFE4', // 亮青色
-          brightWhite: '#F8F8F2', // 亮白色
+          background: '#272822',
+          foreground: '#F8F8F2',
+          cursor: '#F8F8F2',
+          selection: 'rgba(248, 248, 242, 0.3)',
+          black: '#272822',
+          red: '#F92672',
+          green: '#A6E22E',
+          yellow: '#F4BF75',
+          blue: '#66D9EF',
+          magenta: '#AE81FF',
+          cyan: '#A1EFE4',
+          white: '#F8F8F2',
+          brightBlack: '#75715E',
+          brightRed: '#F92672',
+          brightGreen: '#A6E22E',
+          brightYellow: '#F4BF75',
+          brightBlue: '#66D9EF',
+          brightMagenta: '#AE81FF',
+          brightCyan: '#A1EFE4',
+          brightWhite: '#F8F8F2',
         }
       });
 
-      fitAddon = new FitAddon()
+      fitAddon = new FitAddon();
       terminal.loadAddon(fitAddon);
-      terminal.open(this.$refs.terminal)
-
-      this.ws.onmessage = (message) => {
-        // console.log(message)
-        let data = null
+      terminal.open(this.$refs.terminal);
+      window.addEventListener('resize', this.resize);
+      // eslint-disable-next-line vue/no-mutating-props
+      this.ws.onmessage = (event) => {
+        let data = null;
         try {
-          data = JSON.parse(event.data)
-          // fitAddon.fit();
+          data = JSON.parse(event.data);
         } catch (e) {
-          console.error(e)
-          message.showError(this, `JSON数据解析失败：${e.message}`)
+          console.error(e);
+          this.showError(`JSON数据解析失败：${e.message}`);
+          return;
         }
         switch (data.action) {
           case "terminal:output":
-            terminal.write(data.data)
-            break
-          case "terminal:ready":
-            this.terminal_ready()
-            break
+            terminal.write(data.data);
+            break;
         }
-      }
-    },
-    terminal_ready() {
-      window.addEventListener('resize', () => {
-        this.resize()
-      });
-      this.resize()
+      };
+
       terminal.onKey(({key}) => {
-        this.input(key)
+        this.input(key);
       });
       terminal.element.addEventListener('compositionend', (event) => {
         const text = event.data;
-        this.input(text)
+        this.input(text);
       });
-      // 处理键盘按下事件
+
       document.addEventListener('keydown', (e) => {
         if (e.ctrlKey && e.shiftKey && e.code === 'KeyC') {
           document.execCommand('copy');
@@ -118,56 +122,102 @@ export default {
 
         if (e.ctrlKey && e.shiftKey && e.code === 'KeyV') {
           navigator.clipboard.readText().then((text) => {
-            console.log(text);
-            this.input(text)
+            this.input(text);
           });
           e.preventDefault();
         }
       });
-      console.log("ready")
+    },
+    check_login() {
+      // eslint-disable-next-line vue/no-mutating-props
+      this.ws.onmessage = (event) => {
+        let data = null;
+        try {
+          data = JSON.parse(event.data);
+          console.log(data.action)
+        } catch (e) {
+          console.error(e);
+          this.showError(`JSON数据解析失败：${e.message}`);
+          return;
+        }
+        if (data.action === "terminal:ready") {
+          if (data.data) {
+            this.open_terminal();
+            console.log(data.data)
+            this.terminal_ready=data.data
+          }
+        }
+      };
+    },
+    windows_login(){
+      this.send({
+        action: 'terminal:login',
+        data:{
+          'host':1,
+          'port':1,
+          'username':1,
+          'password':1,
+        }
+      })
     },
     resize() {
       fitAddon.fit();
-      const cols = terminal.cols;
-      const rows = terminal.rows;
+      const cols = Math.floor(this.$refs.terminal.offsetWidth / 11);
+      // const cols = 20
+      const rows = Math.floor(this.$refs.terminal.offsetHeight / 18);
+      // const rows = 20
       this.send({
         action: 'terminal:resize',
         data: {
           cols: cols,
           rows: rows
         }
-      })
+      });
     },
     input(key) {
       this.send({
         action: 'terminal:input',
         data: key
-      })
+      });
+    },
+    input_end() {
+      this.send({
+        action: 'terminal:input',
+        data: this.resend
+      });
+      this.resend = '';
     },
     send(data) {
-      this.ws.send(JSON.stringify(data))
+      this.ws.send(JSON.stringify(data));
+    },
+    showError(message) {
+      console.error(message);
     }
   },
-  watch: {
-    online(val) {
-      if (val) {
-        this.open_terminal()
-      }
-    }
-  }
-}
+};
 </script>
 
 <template>
   <v-sheet height="75vh" max-height="800px" width="100%">
     <node-offline-overlay :flag="!online"/>
-    <div id="terminal" ref="terminal" v-if="online" style="height: 100%"></div>
+    <div style="height: 100%; width: 100%; " v-show="terminal_ready">
+        <div id="terminal" ref="terminal" style="height: 85%" ></div>
+        <div style="display: flex; padding: 10px; height: 15%; width: 100%">
+          <v-text-field clearable label="在此处键入命令" variant="outlined" style="flex: 0.80; height: 20%" v-model="resend"></v-text-field>
+          <div style="flex: 0.025"></div>
+          <v-btn prepend-icon="$vuetify" variant="tonal" style="flex: 0.15; height: 80%" @click="input_end">发送</v-btn>
+          <div style="flex: 0.025"></div>
+        </div>
+    </div>
+      <div style="width:100%;height: 100%" v-show="!terminal_ready">
+        <terminalLogin :ws="ws"></terminalLogin>
+      </div>
   </v-sheet>
 </template>
 
 <style scoped>
 @font-face {
-  font-family: 'NotoSansSC-Medium'; /* 自定义字体名称 */
+  font-family: 'NotoSansSC-Medium';
   src: url('/public/fonts/NotoSansSC-Medium.otf');
 }
 
@@ -175,7 +225,7 @@ export default {
   font-family: 'NotoSansSC-Medium', 'Courier New', Courier, monospace;
   font-size: 14px;
   line-height: 1.5;
-  background-color: #272822; /* 背景色为 Monokai 的背景色 */
-  color: #F8F8F2; /* 前景色为 Monokai 的前景色 */
+  background-color: #272822;
+  color: #F8F8F2;
 }
 </style>
