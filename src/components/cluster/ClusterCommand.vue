7c33743d4b77cc941001f6e60f48594b6d1ac309
fsj <fsjasd123456@gmail.com>
2024-07-04 08:54:55 +0800

    fix-ace editor无法加载语言支持问题

--- src/components/cluster/ClusterCommand.vue
+++ src/components/cluster/ClusterCommand.vue
@@ -33,9 +33,10 @@ const props = defineProps({
 })
 
 onMounted(async () => {
+  ace.config.set('basePath', '/node_modules/ace-builds/src-noconflict');
   const options = { // 编辑器设置
     theme: `ace/theme/${props.theme}`, // 主题
-    mode: `ace/mode/${props.lang}`, // 代码匹配模式
+    // mode: `ace/mode/${props.lang}`, // 代码匹配模式
     enableSnippets: true, // 启用代码段
     showLineNumbers: true, // 显示行号
     enableLiveAutocompletion: true, // 启用实时自动完成功能 （比如：智能代码提示）
@@ -45,6 +46,7 @@ onMounted(async () => {
   }
   console.log(options)
   aceEdit = ace.edit(aceRef.value, options)
+  aceEdit.getSession().setMode('ace/mode/sh');
   // 监听
   aceEdit.session.on('change', () => {
     shell.value = aceEdit.getValue()
